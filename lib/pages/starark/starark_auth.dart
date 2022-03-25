531193a32971ea2e5104abc8e2ba28b845399892
Onekki <285079772@qq.com>
2022-03-25 23:48:09 +0800

    pref(web_view): add bottom navbar

--- lib/pages/starark/starark_auth.dart
+++ lib/pages/starark/starark_auth.dart
@@ -5,7 +5,6 @@ import 'package:ok_rush/components/auth_required_state.dart';
 import 'package:ok_rush/pages/web/web_view.dart';
 import 'package:ok_rush/utils/constants.dart';
 import 'package:webview_cookie_manager/webview_cookie_manager.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class StarArkAuthPage extends StatefulWidget {
   const StarArkAuthPage({Key? key}) : super(key: key);
@@ -21,9 +20,7 @@ class _StarArkAuthPageState extends AuthRequiredState<StarArkAuthPage> {
   static const url = "https://$domain/";
 
   final cookieManager = WebviewCookieManager();
-  WebViewController? _controller;
 
-  bool _isLoading = true;
   String? _token;
   String? _cookie;
 
@@ -33,62 +30,35 @@ class _StarArkAuthPageState extends AuthRequiredState<StarArkAuthPage> {
         appBar: AppBar(
           title: const Text("StarArkAuth"),
           actions: [
-            _isLoading
-                ? const SizedBox.shrink()
-                : IconButton(
-                    onPressed: () async {
-                      _cookie = null;
-                      await cookieManager.clearCookies();
-                      context.showSnackBar(message: "cookie cleared");
-                    },
-                    icon: const Icon(Icons.clear_all)),
-            _isLoading
-                ? const SizedBox.shrink()
-                : IconButton(
-                    onPressed: () {
-                      _controller?.reload();
-                    },
-                    icon: const Icon(Icons.refresh)),
-            _isLoading
-                ? const SizedBox.shrink()
-                : IconButton(
-                    onPressed: () async {
-                      _updateTokenAndCookie();
-                      if (_cookie != null) {
-                        Navigator.of(context).pop([_token, _cookie!]);
-                      } else {
-                        context.showErrorSnackBar(message: "cookie get failed");
-                      }
-                    },
-                    icon: const Icon(Icons.check)),
-        ],
-      ),
+            IconButton(
+                onPressed: () async {
+                  _cookie = null;
+                  await cookieManager.clearCookies();
+                  context.showSnackBar(message: "cookie cleared");
+                },
+                icon: const Icon(Icons.clear_all)),
+            IconButton(
+                onPressed: () async {
+                  _updateTokenAndCookie();
+                  if (_cookie != null) {
+                    Navigator.of(context).pop([_token, _cookie!]);
+                  } else {
+                    context.showErrorSnackBar(message: "cookie get failed");
+                  }
+                },
+                icon: const Icon(Icons.check)),
+          ],
+        ),
         body: Stack(
           children: [
             WebViewWidget(
               url: url,
               domain: domain,
               cookie: null,
-              onWebViewCreated: (controller) {
-                _controller = controller;
-              },
-              onPageStarted: (url) {
-                setState(() {
-                  _isLoading = true;
-                });
-              },
               onPageFinished: (url) async {
-                setState(() {
-                  _isLoading = false;
-                });
                 _updateTokenAndCookie();
               },
             ),
-            _isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : const SizedBox.shrink()
           ],
       )
     );
--- lib/pages/web/web_view.dart
+++ lib/pages/web/web_view.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewWidget extends StatefulWidget {
@@ -35,6 +35,9 @@ class WebViewWidget extends StatefulWidget {
 }
 
 class _WebViewWidgetState extends State<WebViewWidget> {
+  WebViewController? _controller;
+  bool _isLoading = true;
+
   @override
   void initState() {
     if (Platform.isAndroid) {
@@ -45,59 +48,118 @@ class _WebViewWidgetState extends State<WebViewWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      debuggingEnabled: kDebugMode,
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebViewCreated: (WebViewController webViewController) async {
-        debugPrint("onWebViewCreated");
-        if (widget.cookie != null) {
-          final cookieMap = Uri.splitQueryString(widget.cookie!);
-          final cookieManager = CookieManager();
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Expanded(
+                child: WebView(
+              debuggingEnabled: kDebugMode,
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) async {
+                debugPrint("onWebViewCreated");
+                if (widget.cookie != null) {
+                  final cookieMap = Uri.splitQueryString(widget.cookie!);
+                  final cookieManager = CookieManager();
 
-          cookieMap.forEach((key, value) async {
-            cookieManager.setCookie(
-              WebViewCookie(name: key, value: value, domain: widget.domain),
-            );
-          });
-        }
-        if (widget.url.startsWith("assets")) {
-          await webViewController.loadFlutterAsset(widget.url);
-        } else {
-          await webViewController.loadUrl(widget.url);
-        }
-        if (widget.onWebViewCreated != null) {
-          widget.onWebViewCreated!(webViewController);
-        }
-      },
-      onProgress: (int progress) {
-        debugPrint('WebView is loading (progress : $progress%)');
-        if (widget.onProgress != null) {
-          widget.onProgress!(progress);
-        }
-      },
-      javascriptChannels: <JavascriptChannel>{
-        JavascriptChannel(
-          name: "Jio",
-          onMessageReceived: (message) {
-            if (widget.onJioCallback != null) {
-              widget.onJioCallback!(message.message);
-            }
-          }
-        )
-      },
-      onPageStarted: (String url) {
-        debugPrint('Page started loading: $url');
-        if (widget.onPageStarted != null) {
-          widget.onPageStarted!(url);
-        }
-      },
-      onPageFinished: (String url) {
-        debugPrint('Page finished loading: $url');
-        if (widget.onPageFinished != null) {
-          widget.onPageFinished!(url);
-        }
-      },
-      backgroundColor: const Color(0x00ffffff),
+                  cookieMap.forEach((key, value) async {
+                    cookieManager.setCookie(
+                      WebViewCookie(
+                          name: key, value: value, domain: widget.domain),
+                    );
+                  });
+                }
+                if (widget.url.startsWith("assets")) {
+                  await webViewController.loadFlutterAsset(widget.url);
+                } else {
+                  await webViewController.loadUrl(widget.url);
+                }
+                _controller = webViewController;
+                if (widget.onWebViewCreated != null) {
+                  widget.onWebViewCreated!(webViewController);
+                }
+              },
+              onProgress: (int progress) {
+                debugPrint('WebView is loading (progress : $progress%)');
+                if (widget.onProgress != null) {
+                  widget.onProgress!(progress);
+                }
+              },
+              javascriptChannels: <JavascriptChannel>{
+                JavascriptChannel(
+                    name: "Jio",
+                    onMessageReceived: (message) {
+                      if (widget.onJioCallback != null) {
+                        widget.onJioCallback!(message.message);
+                      }
+                    })
+              },
+              onPageStarted: (String url) async {
+                debugPrint('Page started loading: $url');
+                setState(() {
+                  _isLoading = true;
+                });
+                if (widget.onPageStarted != null) {
+                  widget.onPageStarted!(url);
+                }
+              },
+              onPageFinished: (String url) async {
+                debugPrint('Page finished loading: $url');
+                setState(() {
+                  _isLoading = false;
+                });
+                if (widget.onPageFinished != null) {
+                  widget.onPageFinished!(url);
+                }
+              },
+              backgroundColor: const Color(0x00ffffff),
+            )),
+            _controller == null
+                ? const SizedBox.shrink()
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () async {
+                          if (_controller != null) {
+                            final canGoBack = await _controller!.canGoBack();
+                            if (canGoBack) {
+                              _controller!.goBack();
+                            }
+                          }
+                        },
+                        icon: const Icon(Icons.arrow_back_ios),
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          if (_controller != null) {
+                            _controller!.reload();
+                          }
+                        },
+                        icon: const Icon(Icons.refresh),
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          if (_controller != null) {
+                            final _canGoForward =
+                                await _controller!.canGoForward();
+                            if (_canGoForward) {
+                              _controller!.goForward();
+                            }
+                          }
+                        },
+                        icon: const Icon(Icons.arrow_forward_ios),
+                      )
+                    ],
+                  ),
+          ],
+        ),
+        _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : const SizedBox.shrink()
+      ],
     );
   }
 }
